8c622fcb23fe36d40b2e8b27894997584d3f1450
Sebastian Sanchez <sebastian.sanchezp@usm.cl>
2025-08-22 21:43:39 +0200

    tb fixed

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  Adder_test_SS user_project (
+  tt_um_Adder_test_SS user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
