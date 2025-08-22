710022d6ad278e489183f273d15163eaafe9b61d
Sebastian Sanchez <sebastian.sanchezp@usm.cl>
2025-08-22 20:49:31 +0200

    Fixed commit

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module Adder_test_SS (
+module tt_um_Adder_test_SS (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
